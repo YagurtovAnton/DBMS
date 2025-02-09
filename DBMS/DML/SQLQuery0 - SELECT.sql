72a7b8a1a9e582e34f3246e6708f176e8b9b4a7e
YagurtovAnton <Yagur_oo18@top-academy.ru>
2025-02-09 20:00:12 +0300

    DML

---
+++ DBMS/DML/SQLQuery0 - SELECT.sql
@@ -0,0 +1,12 @@
+?USE PV_319_DDL;
+GO
+
+SELECT * FROM Students;
+			--last_name	AS N'Фамилия',
+			--first_name	AS N'Имя',
+			--middle_name AS N'Отчество',
+			--last_name + ' ' + first_name + ' ' + middle_name AS N'Ф.И.О. студента',
+			--birth_date		AS N'Дата рождения',
+			--[group_name]	AS N'Группа',
+			--direction_name	AS N'Направление обучения'
+			--Students, Groups, Directions
