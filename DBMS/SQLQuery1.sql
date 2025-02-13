6a1098ad5ad3ed78e81914af9df888e7ebf4d1eb
YagurtovAnton <Yagur_oo18@top-academy.ru>
2025-02-13 21:30:35 +0300

    INSERT/SELECT Schedule DONE

---
+++ DBMS/SQLQuery1.sql
@@ -0,0 +1,5 @@
+USE PV_319_Import;
+GO
+
+--SELECT	group_id			FROM Groups				WHERE group_name		 = N'PV_319'
+SELECT	discipline_id		FROM Disciplines		WHERE discipline_name	 LIKE N'Объектно-ориентированное программирование%';
