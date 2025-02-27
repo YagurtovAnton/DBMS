fb05188bc4ac390c7eb1695fb2aca2d4b1c11d49
YagurtovAnton <Yagur_oo18@top-academy.ru>
2025-02-27 22:34:08 +0300

    TODO Holodays

--- DBMS/T-SQL/SQLQuery2 - Schedule Procedures test.sql
+++ DBMS/T-SQL/SQLQuery2 - Schedule Procedures test.sql
@@ -1,6 +1,14 @@
 USE PV_319_Import;
 GO
 
-EXEC sp_ScheduleForStacionarGrops N'PV_319', N'%UML%', N'Ковтун' , N'2024-08-10', N'18:30';
+--EXEC sp_ScheduleForStacionarGrops N'PD_321', N'%MS SQL Server%', N'Покидюк' , N'2024-06-01', N'13:30';
 
-EXEC sp_PrintScheduleForGroup N'PV_319', N'%C++%';
+--EXEC sp_PrintScheduleForGroup N'PD_321';
+--EXEC sp_PrintScheduleForGroupPeriod N'PV_319', N'2024-06-01',N'2024-07-01';
+--DECLARE @salary SMALLMONEY;
+--EXEC @salary =  sp_CountTeahersSalary N'Ковтун', 2024, 06;
+--PRINT (@salary);
+--EXEC sp_ScheduleForBaseStacionarGroup N'PV_319', N'Процедурное программирование %', N'Ковтун', N'2023-11-23', '18:30', 2,4,6,2,1;
+--EXEC sp_ScheduleForBaseStacionarGroup N'PV_319', N'Процедурное программирование %', N'Ковтун', N'2023-11-23', '18:30',4,2,1;
+--EXEC sp_ScheduleForBaseStacionarGroup N'PV_319', N'Hardware%', N'Кобылинский', N'2023-11-25', '18:30', 6,2,0;
+EXEC sp_PrintScheduleForGroup N'PV_319';
